2392f2d30c5b26b8a74428eae21741eeb4f209d1
Ahmed Alaa <ahmed.alla56756@gmail.com>
2024-05-24 20:59:41 +0300

    adding home button to products page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,6 +2,19 @@
 
 
 @section('content')
+
+    <div class="mt-2 ms-5">
+        <a class="btn btn-secondary btn-hover-green fw-medium" href='/'>Home</a>
+    </div>
+
+    <style>
+        .btn-hover-green:hover {
+            background-color: #164863;
+            border-color: #164863;
+        }
+    </style>
+
+
     @if ($products->isEmpty())
         <div class="container text-center">
             <h1>No products available for this category.</h1>
